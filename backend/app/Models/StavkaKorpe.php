a092ae928306f68da28cf1fc1ecdb959cc416b66
Đurđica Jovanović <jovanovic.djurdjica@hmail.com>
2023-02-15 22:42:50 +0100

    kreiranje modela

---
+++ backend/app/Models/StavkaKorpe.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class StavkaKorpe extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'proizvod_id',
+        'korpa_id',
+        'kolicina',
+        
+    ];
+    public function korpa()
+    {
+        return $this->belongsTo(Korpa::class);
+    }
+    public function proizvod(){
+        return $this->belongsTo(Proizvod::class);
+    }
+}
