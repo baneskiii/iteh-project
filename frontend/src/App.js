b13db590f6fdd4b1ac3f7006d3756511ff860e88
Jovana <jovanalazarevic394@gmail.com>
2023-02-17 14:55:51 +0100

    Napravljena pocetna stranica i footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import Footer from './Components/Footer';
 import NavBar from './Components/NavBar';
+import Pocetna from './Components/Pocetna';
 
 function App() {
   return (
   
     <div  >
     <NavBar></NavBar>
+    <Pocetna></Pocetna>
+    <Footer></Footer>
 </div>
 
   );
--- frontend/src/Components/Pocetna.jsx
+++ frontend/src/Components/Pocetna.jsx
@@ -1,7 +1,37 @@
-function Pocetna() {
+import React from 'react';
+import { Slide } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css'
+
+const slideImages = [
+    'https://www.sportvision.rs/files/images/2023/2/14/SV%20nova%20kolekcija%20img%20JAN23%201200x526.jpg',
+    'https://www.sportvision.rs/files/images/2023/1/20/SV-ski-desktop%281%29.jpg',
+    'https://www.sportvision.rs/files/images/2022/11/30/SV-winter-desktop.jpg'
+  ];
+
+  const Pocetna = () => {
+
     return (
-      <div className="App">
+
+      <div> 
+        <Slide easing="ease">
+          <div className="each-slide">
+            <div style={{'backgroundImage': `url(${slideImages[0]})`}}>
+
+            </div>
+          </div>
+          <div className="each-slide">
+            <div style={{'backgroundImage': `url(${slideImages[1]})`}}>
+
+            </div>
+          </div>
+          <div className="each-slide">
+            <div style={{'backgroundImage': `url(${slideImages[2]})`}}>
+
+            </div>
+          </div>
+        </Slide>
       </div>
-    );
-  }
-  export default Pocetna;
+    )
+};
+
+ export default Pocetna;
