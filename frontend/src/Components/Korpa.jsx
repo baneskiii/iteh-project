339096b8ccd7bf98e3fd2745f283d387b8ab6b28
Jovana <jovanalazarevic394@gmail.com>
2023-02-18 17:34:22 +0100

    Izbacivanje iz korpe

--- frontend/src/Components/Korpa.jsx
+++ frontend/src/Components/Korpa.jsx
@@ -1,18 +1,18 @@
 import React, { useState } from 'react'
-import OpremaKartica from './OpremaKartica'
+import {BsPlusLg, BsDashLg} from "react-icons/bs"
 
 function Korpa({oprema,onAdd,onRemove }) {
 
   return (
     <div> 
-        <div className='svaOprema'>
+        <div className='korpa'>
 
             <table>
                 <thead>
-                    <tr><th>ID</th><th>NAZIV</th><th>CENA</th><th>KOLICINA</th></tr>
+                <tr><th>ID</th><th>NAZIV</th><th>CENA</th><th>KOLICINA</th><th>DODAJ</th><th>IZBACI</th></tr>
                 </thead>
                 <tbody>
-                        {oprema.map((o)=>(<tr><td>{o.id}</td><td>{o.naziv}</td><td>{o.cena}</td><td>{o.kolicina}</td></tr>))}
+                        {oprema.map((o)=>(<tr><td>{o.id}</td><td>{o.naziv}</td><td>{o.cena}</td><td>{o.kolicina}</td><td><button className="btn" onClick={() => onAdd(o.id)}><BsPlusLg/></button></td><td><button className="btn" onClick={() => onRemove(o.id)}><BsDashLg/></button></td></tr>))}
                 </tbody>
             </table>
 
